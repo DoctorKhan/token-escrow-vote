115f74eaf6599a8d63c7b22ac6f43858db33e355
Nick Geoca <nickgeoca@gmail.com>
2018-01-13 16:41:35 -0600

    Update migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var SafeMath = artifacts.require('SafeMath.sol');
-var SimpleStorage = artifacts.require('SimpleStorage.sol');
+var SimpleStorage = artifacts.require('EscrowFactory.sol');
 
 module.exports = function(deployer, network, accounts) {
   return deployer.deploy(SafeMath).then(async () => {
 
-    await deployer.deploy(SimpleStorage);
+    await deployer.deploy(EscrowFactory);
 
   });
 }
