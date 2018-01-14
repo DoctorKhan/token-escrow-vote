b6f3827a3f7af64e85b7b539df3a5d9ea3d1a6df
Nick Geoca <nickgeoca@gmail.com>
2018-01-14 15:17:06 -0600

    start of first test

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,8 +1,10 @@
-// var events = require('./../app/javascripts/events');
+var events = require('./../app/javascripts/events');
 // var util = require('./../app/javascripts/util');
 
 var EscrowFactory = artifacts.require('EscrowFactory.sol');
 var Escrow = artifacts.require('Escrow.sol');
+var MainToken = artifacts.require('MockMainToken.sol');
+var MiniMeTokenFactory = artifacts.require(' MockMiniMeTokenFactory.sol');
 
 var chai = require('chai')
 const assert = require("chai").use(require("chai-as-promised")).assert;
@@ -22,17 +24,34 @@ contract('Escrow', function (accounts) {
   var escrowFactory;
 
   describe('works with minime token', async () => {
+    var minimetoken;
+
     beforeEach(async () => {
       escrowfactory = await EscrowFactory.new({from: account1});
-      
+      const mmtf = await MiniMeTokenFactory.new({from: account1});
+
       var numRounds  = new BigNumber(3);
       var controller = account1;
-      var token = ;
-      escrow = await escrowFactory.createEscrow(numRounds, controller, token, {from: account1});
+      minimetoken = MainTokken.new(mmtf.address, {from: account1});
+
+      //todo fix this
+      escrow = await escrowFactory.createEscrow(numRounds, controller, minimetoken.address, {from: account1});
+
     });
 
-    it('fails on fallback', async () => {
-      
+    it('notifies us an escrow was created', async () => {
+      escrow = await escrowFactory.createEscrow(numRounds, account2, minimetoken.address, {from: account1});
+      var tx = undefined;
+      const x = events.EscrowCreation(tx);
+
+      const escrow = x[0];     // address
+      assert.equal(escrow, '0x0');
+
+      const controller = x[1]; // address
+      assert.equal(controller, account2);
+
+      const token = x[2];      // address
+      assert.equal(token, minimetoken.address);
     });
   });
 });
