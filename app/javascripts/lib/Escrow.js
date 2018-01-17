172f4422c0659a9ff055b339f25fa01a1d206a84
Nick Geoca <nickgeoca@gmail.com>
2018-01-17 15:55:47 -0600

    interface is singleton. remove factory visibility

--- app/javascripts/lib/Escrow.js
+++ app/javascripts/lib/Escrow.js
@@ -1,10 +1,54 @@
 var escrow = undefined;
+var escrowFactory = undefined;
+
+// =============
+// Init function
+// =============
+
+var init = async(numRounds, controller, token) => {
+  let promise = await new Promise((resolve, reject) => {
+    if (simulated) resolve();
+    
+    // use mock if testrpc
+    if (testrpc) {
+      var EscrowFactory = artifacts.require('MockEscrowFactory.sol');
+      var Escrow = artifacts.require('MockEscrow.sol');
+    } else {
+      var EscrowFactory = artifacts.require('EscrowFactory.sol');
+      var Escrow = artifacts.require('Escrow.sol');
+    }
+
+    if (escrowFactory == undefined) {
+      escrowFactory = await MetaCoin.deployed();
+      // check code safety here. dont want user to accidently deploy factory
+      if (ef.address == undefined)
+        escrowFactory = await EscrowFactory.new({from: account1});
+    }
+
+    var event = escrowFactory.events.EscrowCreation({filter: {controller: account1}});
+      
+    var listener = async(result) => {
+      escrow = await Escrow.at(result.escrow);
+      resolve();
+    }
+      
+    event.once('data', listener);
+    event.once('error', e => reject(e));
+      
+    const tx = await escrowFactory.createEscrow(numRounds, controller, token)
+            .send({from: account1})
+            .catch(e => reject(e));
+
+  return promise;
+}
 
 // ==============
 // User functions
+// ==============
 
 // run this during token sale
 var allocVotes = async() => {
+  if (simulated) return;
   if (escrow == undefined) throw('Escrow undefined');
 
   const tx = await escrow.allocVotes(); 
@@ -19,34 +63,31 @@ var allocVotes = async() => {
 // * what if oracle fails b/c gas too small? no refund?
 // * set account. default to account1? does metamask provide a hook to change it?
 var getRefund = async() => {
-    let promise = await new Promise((resolve, reject) => {
-      if (escrow == undefined) reject('Escrow undefined');
-      if (simulated) resolve('3.0023427');
-  
-      var event = escrow.events.RefundAmount({filter: {user: account1}});
-      
-      var refundAmount;
-      var listener = function(result) {
-        resolve(result.returnValues.refundAmount.toNumber(10));
-      }
-      
-      event.once('data', listener);
-      event.once('error', e => reject(e));
-      
-      const tx = await escrow.refund()
-            .send({from: account1})
-            .catch(e => reject(e));
-      
-    })
-        .catch(err => {throw err});
-  
+  let promise = await new Promise((resolve, reject) => {
+    if (simulated) resolve('3.0023427');
+    if (escrow == undefined) reject('Escrow undefined');
+    
+    var event = escrow.events.RefundAmount({filter: {user: account1}});
+    
+    var refundAmount;
+    var listener = function(result) {
+      resolve(result.returnValues.refundAmount.toNumber(10));
+    }
+    
+    event.once('data', listener);
+    event.once('error', e => reject(e));
+    
+    const tx = await escrow.refund()
+          .send({from: account1})
+          .catch(e => reject(e));
+    
   return promise;
 }
 
 // vote yes or no
 var singleVote = async(yes) => {
-  if (escrow == undefined) throw('Escrow undefined');
   if (simulated) return;
+  if (escrow == undefined) throw('Escrow undefined');
 
   const tx = await escrow.singleVote(yes);
 }
@@ -58,15 +99,18 @@ var singleVote = async(yes) => {
 
 // todo what are param types? string? num? bigNum?
 var setRoundWindow = async(roundNum, start, end) => {
-  if (escrow == undefined) throw('Escrow undefined');
   if (simulated) return;
+  if (escrow == undefined) throw('Escrow undefined');
 
   cosnt tx = await escrow.setRoundWindow(roundNum, start, end).send({from: account1})
 }
 
-// ===
-// API
-// ===
+// =================
+//       API
+// =================
+
+// init
+exports.init = init;
 
 // User
 exports.getRefund = getRefund;
