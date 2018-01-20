8d54c8527ba22ec4a144d585835ac039ff910ca9
Nick Geoca <nickgeoca@gmail.com>
2018-01-20 00:38:20 -0600

    All escrows in one contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -4,96 +4,97 @@ pragma solidity ^0.4.15;
    3. should refund if 
       a. tokens passed and b. no vote and c. company agrees.
  */
+
+    // allow revoting?
+  // should we include self destruct after usefulness of escrow has expired?
+
 import "../../MiniPre/contracts/ERC20.sol";
 
 contract Escrow {
-  uint public numVotes; //this can be a functions.. yesVotes+noVotes
-  //  uint public yesVotes;
-  //  uint public noVotes;
-  uint public roundNum;
-  uint public numRounds;
-  address public company;
-  uint public baseExchange;
+
+  // ======
+  // TYPES:
+  // ======
 
   struct Fraction {
-    uint num;
+    uint numer;
     uint denom;
   }
 
-  // threshold approval
-  uint public threshNum;
-  uint public threshDen;
-  uint public minVotes;
-  ERC20 public tokenContract;
-  
-  // allow revoting?
-  mapping (uint => mapping (address => bool)) public hasVoted; // round -> user -> hasVoted
-  mapping (uint => uint) public funds2beReleased;              // round -> amount-sent-to-company
-  mapping (uint => bool) public roundOpen;                     // round -> closed/open
-  mapping (uint => uint) public endTime;                       // round -> vote-end-time
-  mapping (uint => uint) public startTime;                     // round -> vote-start-time
-  mapping (uint => uint) public yesVotes;                      // round -> yes-votes
-  mapping (uint => uint) public noVotes;                       // round -> no-votes  
-  mapping (address => uint) public voteWeight;                 // user  -> vote-weight
+  struct RoundData {
+    uint funds2beReleased;
+    uint endTime;
+    uint startTime;
+    uint yesVotes;
+    uint noVotes;
+    mapping (address => bool) hasVoted; // user -> hasVoted
+  }
 
+  struct EscrowData {
+    address company;
+    address arbitrator;
+    ERC20 tokenContract;
+    address payoutAddress;
+
+    uint roundNum;
+    uint numRounds;
+    uint baseExchange;
+    bool arbitratorApproved;
+
+    Fraction thresholdApproval; // should look this over
+    uint minVotes;
+    mapping (address => uint) voteWeight;   // user -> vote-weight
+
+    mapping (uint => RoundData) round;
+  }
+
+  mapping (bytes24 => EscrowData) escrows;   // id -> escrow
+  
   event VotingResult(bool indexed releasedFunds);
+  event RefundAmount(address indexed user, uint refundAmount);
+  event EscrowCreation(address indexed company, bytes24 id);
 
-  function Escrow(uint _numRounds, address _controller, address _token) {
-    tokenContract = ERC20(_token);
-    numRounds = _numRounds;
+  function Escrow() { }
+
+  // =====================
+  // ARBITRATOR & COMPANY:
+  // =====================
+
+  // ===========
+  // ARBITRATOR:
+  // ===========
+
+  function aribtratorApprove(bytes24 id) isArbitrator(id) public { 
+    escrows[id].arbitratorApproved = true; 
   }
-
-  // should we include self destruct after usefulness of escrow has expired?
-
-  // fix this
-  function sqrt(uint a) returns (uint) { return 1; }
-
-  // consider a minimum voting power fn = sqrt(userToken) / SIGMA_u sqrt(u)
-  // could be really useful. would have to be calculate offchain
-  function minVotingPower(address user) public constant returns (string) {
-    // return alloc[user] / maxVoteCount;
-    return "1.421%";
-  }
-
-  // consider privledges
-  function startVoteRound() public {
-    require(getBlockTime() >= startTime[roundNum + 1] && getBlockTime() <= endTime[roundNum + 1]); // make sure in voting window
-    roundNum = roundNum + 1;
-  }
-
-  function allocVotes() public {
-    uint tokenNum = tokenContract.balanceOf(msg.sender);
-    voteWeight[msg.sender] = sqrt(tokenNum); 
-  }
-  
-  // voting based on balances at certain point in BC? e.g. minime token? consider people not using it
-  function singleVote(bool votedYes) public {
-    // Error check
-    require(hasVoted[roundNum][msg.sender] == false);
-    require(getBlockTime() >= startTime[roundNum] && getBlockTime() <= endTime[roundNum]); // make sure in voting window
-
-    // State changes
-    if (votedYes) {
-      yesVotes[roundNum] = yesVotes[roundNum] + voteWeight[msg.sender];
-    }
-    else {
-      noVotes[roundNum] = noVotes[roundNum] + voteWeight[msg.sender];
-    }
-    numVotes++;
-    hasVoted[roundNum][msg.sender] = true;
-   }
-
-  // ======
-  // ADMIN:
-  // ======
-
 
   // ========
   // COMPANY:
   // ========
-  
-  //
-  // function () isController {  }
+
+  function createEscrow(uint numRounds, address arbitrator, address token, address payoutAddress) {
+    address company = msg.sender;
+    bytes24 id = 0; // sha3(company, arbitrator, token, payoutAddress);
+
+    escrows[id].company = company;
+    escrows[id].arbitrator = arbitrator;
+    escrows[id].tokenContract = ERC20(token);
+    escrows[id].payoutAddress = payoutAddress;
+    escrows[id].numRounds = numRounds;
+
+    EscrowCreation(company, id);
+  }
+
+  // consider privledges. arbitrator?
+  function startVoteRound(bytes24 id)  isArbitrator(id) isCompany(id) public {
+    uint startTime = escrows[id].round[roundNum + 1].startTime;
+    uint endTime = escrows[id].round[roundNum + 1].endTime;
+    uint roundNum = escrows[id].roundNum;
+
+    // make sure in voting window
+    require(getBlockTime() >= startTime && getBlockTime() <= endTime); 
+    escrows[id].roundNum = escrows[id].roundNum + 1;
+  }
 
   // open voting round
   function openVote(uint round) public {
@@ -107,10 +108,17 @@ contract Escrow {
   }
 
   // big thing here, make sure this fn is safe if roundNum changes, or don't let roundNum change
-  function thresholdReached() public constant returns (bool) {
+  function thresholdReached(bytes24 id) public constant returns (bool) {
+    uint roundNum = escrows[id].roundNum;
+    uint yesVotes = escrows[id].round[roundNum].yesVotes;
+    uint noVotes = escrows[id].round[roundNum].noVotes;
+    uint totalVotes = yesVotes + noVotes;
+    uint denom = escrows[id].thresholdApproval.denom;
+    uint numer = escrows[id].thresholdApproval.numer;
+
     // this should be related to noVotes too. Or, what if voting is only 10% turnout? this is valid, but might be rejected b/c didn't reach threshold
-    return numVotes > minVotes
-        && (yesVotes[roundNum] * threshDen) > threshNum;
+    return totalVotes > escrows[id].minVotes
+        && (yesVotes * denom) > numer;
   }
 
   // keep track of failures
@@ -119,17 +127,16 @@ contract Escrow {
   }
 
   // releases funds to company
-  function releaseFunds() internal {
-    require(company.send(funds2beReleased[roundNum]));
-    // require so it doesn't eat gas
+  function releaseFunds(bytes24 id) internal {
+    require(escrows[id].payoutAddress.send(escrows[id].round[escrows[id].roundNum].funds2beReleased));
   }
 
-  // isController ?
-  function setRoundWindow(uint roundNum, uint start, uint end) public  {
+  function setRoundWindow(bytes24 id, uint roundNum, uint start, uint end) isCompany(id) public  {
     require(start < end);
+    var round = escrows[id].round[roundNum];
 
-    startTime[roundNum] = start;
-    endTime[roundNum]   = end;
+    round.startTime = start;
+    round.endTime = end;
   }
 
   // todo. make fallback fail
@@ -139,6 +146,68 @@ contract Escrow {
   // =====  
   // note. consider where tokens go. if tokens go to company, then they can cheat system. if tokens go to this contract, then they are stuck here unless we send them back to company
 
+  function allocVotes(bytes24 id) public {
+    var userVoteWeight = escrows[id].voteWeight[msg.sender];
+    uint tokenNum = escrows[id].tokenContract.balanceOf(msg.sender);
+    
+    userVoteWeight = sqrt(tokenNum); 
+  }
+
+
+  // Require approval of entire balanceOf?
+  function refund(bytes24 id) public {
+    // add this    require(inRefundState);
+    
+    // Get tokens, then refund remaining ether
+
+    /*
+    uint tokenCount = escrows[id].tokenContract.balanceOf(msg.sender);
+    uint refundAmount = tokenCount * getExchangeRate();
+    // uint refundAmount = tokenCount * currentExchangeRate;  // Num and denom style is probably better. Probably need be careful in what gets divided and multiplied first.. e.g. (1/4)*8 vs (1*8)/4
+    require(tokenContract.transferFrom(msg.sender, this, tokenCount));  // make sure params are right
+    msg.sender.send(refundAmount);
+    RefundAmount(msg.sender, refundAmount);
+    */
+  }
+
+  // voting based on balances at certain point in BC? e.g. minime token? consider people not using it
+  function singleVote(bytes24 id, bool votedYes) public {
+    // Error check
+    uint roundNum = escrows[id].roundNum;
+    var escrowRound = escrows[id].round[roundNum];
+
+    require(escrowRound.hasVoted[msg.sender] == false);
+    require(getBlockTime() >= escrowRound.startTime && getBlockTime() <= escrowRound.endTime); // make sure in voting window
+
+    // State changes
+    if (votedYes) { // safe math? 
+      escrowRound.yesVotes = escrowRound.yesVotes + escrows[id].voteWeight[msg.sender];
+    }
+    else {
+      escrowRound.noVotes = escrowRound.noVotes + escrows[id].voteWeight[msg.sender];
+    }
+    escrowRound.hasVoted[msg.sender] = true;
+  }
+  
+
+  // =====
+  // MISC:
+  // =====
+  // Use these times for testing
+  
+  function getBlockTime()   internal constant returns  (uint) { return block.timestamp; }
+  function roundOpen(bytes24 id) returns (bool)  {return true;}
+
+  // fix this
+  function sqrt(uint a) returns (uint) { return 1; }
+
+  // consider a minimum voting power fn = sqrt(userToken) / SIGMA_u sqrt(u)
+  // could be really useful. would have to be calculate offchain
+  function minVotingPower(address user) public constant returns (string) {
+    // return alloc[user] / maxVoteCount;
+    return "1.421%";
+  }
+
   function getExchangeRate() returns (uint){
     // minimi previous balance, oracle token discount, etc
     // minime balanceOfAt(msg.sender,  _blockNumber)
@@ -146,39 +215,17 @@ contract Escrow {
     // tokeRatio should be between 0 and 2
     uint tokenRatio = 1;
     
-    return baseExchange * tokenRatio;
+    return 1; //return baseExchange * tokenRatio;
   }
-
-  event RefundAmount(address indexed user, uint refundAmount);
-
-  // Require approval of entire balanceOf?
-  function refund() public {
-    // add this    require(inRefundState);
-    
-    // Get tokens, then refund remaining ether
-    
-    uint tokenCount = tokenContract.balanceOf(msg.sender);
-    uint refundAmount = tokenCount * getExchangeRate();
-    // uint refundAmount = tokenCount * currentExchangeRate;  // Num and denom style is probably better. Probably need be careful in what gets divided and multiplied first.. e.g. (1/4)*8 vs (1*8)/4
-    require(tokenContract.transferFrom(msg.sender, this, tokenCount));  // make sure params are right
-    msg.sender.send(refundAmount);
-    RefundAmount(msg.sender, refundAmount);
-  }
-
-  // =====
-  // MISC:
-  // =====
-  modifier isCompany() {
-    require(msg.sender == company);
-    _;
-  }
-
-  modifier isAdmin() {  // should this be a controlled contract? isController might be same
-    require(msg.sender == company);
-    _;
-  }
-
-  // Use these times for testing
   
-  function getBlockTime()   internal constant returns  (uint) { return block.timestamp; }
+  modifier isArbitrator (bytes24 id) { 
+    require(msg.sender == escrows[id].arbitrator); 
+    _; 
+  }
+
+  modifier isCompany (bytes24 id) { 
+    require(msg.sender == escrows[id].company); 
+    _;
+  }
+
 }
